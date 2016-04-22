3bfb429dcdb216313b11d464882b2c83b1c80c16
unenternow <manlove0001@gmail.com>
2016-04-22 09:26:26 +0700

    update new feed

--- application/views/home/content2.php
+++ application/views/home/content2.php
@@ -5,8 +5,7 @@
       <table border = '1' width = '80%' align = 'center'>
 		<?php foreach($query->result() as $row) { ?>	
 				<tr align = 'left'>
-					<td><h5>&nbsp;&nbsp;&nbsp;<a href="<?php echo site_url('template/inweb3/'.$row->report_id); ?>">รายงานการประชุมครั้งที่ : <?php echo $row->no; ?></a></td></br>
-					
+					<td><h5>&nbsp;&nbsp;&nbsp;<a href="<?php echo site_url('template/inweb3/'.$row->report_id); ?>">รายงานการประชุมครั้งที่ : <?php echo $row->no; echo ($row->artifact == 1)? " <img src='http://www.apr.ubru.ac.th/images/stories/imgweb/img_news/icon/new-icon.gif' width='30'></img>" : ""; ?></a></td></br>
 					<?php if(isset($_SESSION['sys_login'])&&$_SESSION['sys_login']==true && $_SESSION['intype'] == 1) { ?>
 							<td><a href="<?php echo site_url('template/editreport/'.$row->report_id); ?>">แก้ไข</a></td>
 							<td><a href="<?php echo site_url('template/deletereport/'.$row->report_id); ?>">ลบ</a></td>
